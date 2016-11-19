36bf4a3f479387fd009a9fa42170f4584c4ef104
VirginieGodfrin <virginiegodfrin@gmail.com>
2016-11-19 15:42:04 +0100

    bootstrap & js-datepicker & {{form_row(...)}}

--- src/AppBundle/Form/GenusFormType.php
+++ src/AppBundle/Form/GenusFormType.php
@@ -6,14 +6,21 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use AppBundle\Entity\SubFamily;
+use AppBundle\Repository\SubFamilyRepository;
 
 class GenusFormType extends AbstractType {
 
 	public function buildForm(FormBuilderInterface $builder, array $options) {
 			$builder
 				->add('name') 
-				->add('subFamily', null, [
-					'placeholder'=>'choose a sub Family'
+				->add('subFamily', EntityType::class, [
+					'class' => SubFamily::class,
+					'query_builder'=> function(SubFamilyRepository $repo){
+						return $repo->createAlphabeticalQueryBuilder();
+					} 
 				]) 
 				->add('speciesCount') 
 				->add('funFact') 
@@ -23,7 +30,13 @@ class GenusFormType extends AbstractType {
 						'No'=>false,
 					]
 				]) 
-				->add('firstDiscoveredAt')
+				->add('firstDiscoveredAt', DateType::class,[
+						'widget' => 'single_text',
+						'attr' => [
+							'class' => 'js-datepicker'
+						],
+						'html5' => false,
+				])
 			;
 	}
 
