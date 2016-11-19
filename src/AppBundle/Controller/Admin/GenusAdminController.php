5457f462f0471a9a5bb7d29003a842499e777c9d
VirginieGodfrin <virginiegodfrin@gmail.com>
2016-11-19 16:58:24 +0100

    edit action

--- src/AppBundle/Controller/Admin/GenusAdminController.php
+++ src/AppBundle/Controller/Admin/GenusAdminController.php
@@ -8,6 +8,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Form\GenusFormType;
 use Symfony\Component\HttpFoundation\Request;
 
+use AppBundle\Entity\Genus;
+
 /**
  * @Route("/admin")
  */
@@ -49,6 +51,33 @@ class GenusAdminController extends Controller
         }
 
         return $this->render('admin/genus/new.html.twig',[
+            'genusForm'=>$form->createView()
+            ]
+        );
+    }
+
+    /**
+     * @Route("/genus/{id}/edit", name="admin_genus_edit")
+     */
+    public function editAction(Request $request, Genus $genus){
+
+        $form = $this->createForm(GenusFormType::class, $genus);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid() ){
+            
+            $genus = $form->getData();
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($genus);
+            $em->flush();
+
+            $this->addFlash('success', 'genus updated super star!');
+
+            return $this->redirectToRoute('admin_genus_list');
+        }
+
+        return $this->render('admin/genus/edit.html.twig',[
             'genusForm'=>$form->createView()
             ]
         );
